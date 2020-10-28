334e6b1418c0c5f9f6f6a89191684ef882e96fd2
ryry <ryanpetrecca@gmail.com>
2020-10-28 14:53:53 -0700

    added some quality comments for clarity later

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function setTime() {
         sendMessage();
       }
   
-    }, 1000);
+    }, 1000); //This number here is in milliseconds so every 1000 ms = 1 second
   }
-
+  //This is calling on the countdown timer function, will put this in with a button that starts the quiz
   setTime();
